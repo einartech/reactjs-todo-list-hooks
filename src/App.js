3bca3be002e078f9f0836f00813efcdd25519978
Re-On <reonmediachannel@gmail.com>
2020-12-28 21:26:53 +0100

    To-Do List App react Course video num 10

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import TareaForm from "./componentes/TareaForm";
+import Tarea from "./componentes/Tarea";
+import "./App.css";
+
+function App() {
+  const [listaTareas, setListaTareas] = useState([]);
+
+  const nuevaTarea = (tarea) => {
+    setListaTareas([tarea, ...listaTareas]);
+  };
+
+  const borrar = (id) => {
+    const listaFiltrada = listaTareas.filter((e, index) => index !== id);
+    setListaTareas(listaFiltrada);
+  };
+
+  const actualizarTarea = (id, tarea) => {
+    const listaActualizada = listaTareas.map((e, index) => {
+      if (index === id) {
+        e = tarea;
+      }
+
+      return e;
+    });
+
+    setListaTareas(listaActualizada);
+  };
+
+  return (
+    <div className="App">
+      <TareaForm nuevaTarea={nuevaTarea} />
+
+      <div className="lista">
+        {listaTareas.map((e, index) => (
+          <Tarea
+            tarea={e}
+            borrar={borrar}
+            id={index}
+            editar={actualizarTarea}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
